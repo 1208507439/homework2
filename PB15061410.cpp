46d449a2e0b6d82f0c3fbc140b6d327a37df6512
FishHawk <32151331+FishHawk@users.noreply.github.com>
2017-10-12 15:58:55 +0800

    Update PB15061410.cpp

--- PB15061410.cpp
+++ PB15061410.cpp
@@ -1,13 +1,202 @@
 #include "CircleFinder.h"
 
-#define VERSION_5
+#define VERSION_6
 
 const int MIN_RASIUS = 5;
-const int MAX_RASIUS = 500;
+const int MAX_RASIUS = 1000;
 
 const int MAX_CIRCLE_NUMBER = 1000;
 
 const int STEP = 5;
+
+#ifdef VERSION_6
+/*
+VERSION 6.0
+Loop order r-theta-x-y
+Based on opencv
+Lossy optimization!!!
+Same circle have removed, AT THE END
+*/
+int ustc_Find_Circles_By_Difference(
+	Mat colorImg,
+	int min_radius,
+	int max_radius,
+	int min_center_dist,
+	int min_radius_dist,
+	int max_circle_diff,
+	int* x,
+	int* y,
+	int* radius,
+	int* circle_cnt,
+	int max_circle)
+{
+	if (colorImg.data == NULL) return MY_FAIL;
+
+	const int rows = colorImg.rows;
+	const int cols = colorImg.cols;
+
+	if (min_radius < MIN_RASIUS) return MY_FAIL;
+	if (min_radius > max_radius) return MY_FAIL;
+	if (max_radius > MAX_RASIUS) return MY_FAIL;
+
+	if (max_radius > rows / 2 - STEP) max_radius = rows / 2 - STEP;
+	if (max_radius > cols / 2 - STEP) max_radius = cols / 2 - STEP;
+	if (min_radius > rows / 2 - STEP) min_radius = rows / 2 - STEP;
+	if (min_radius > cols / 2 - STEP) min_radius = cols / 2 - STEP;
+
+	if (min_center_dist < 0) return MY_FAIL;
+	if (min_radius_dist < 0) return MY_FAIL;
+	if (max_circle_diff <= 0 || max_circle_diff > 255) return MY_FAIL;
+
+	if (max_circle <= 0) return MY_FAIL;
+
+	int _circle_cnt = 0;
+	int* diff = new int[max_circle];
+
+	//Origin Image
+	Mat originImg;
+	colorImg.convertTo(originImg, CV_16UC3);
+	originImg = originImg * 0.7;
+
+	//Diff Threshold Image
+	Mat diffThresholdImg;
+	diffThresholdImg.create(rows, cols, CV_8UC1);
+	diffThresholdImg.setTo(max_circle_diff);
+	diffThresholdImg = diffThresholdImg * 0.7;
+	diffThresholdImg.convertTo(diffThresholdImg, CV_16UC1);
+
+
+	//Average Image
+	Mat aveImg[MAX_RASIUS + STEP + 1];
+
+	//Diff Image
+	Mat diffImg[MAX_RASIUS + STEP + 1];
+
+	//Calculate Diff Image
+	for (int r = min_radius - STEP; r <= max_radius + STEP; r++)
+	{
+		Mat sumImg;
+		sumImg.create(rows, cols, CV_16UC3);
+		sumImg.setTo(0);
+
+
+
+		int rows_start_table[360], rows_end_table[360], cols_start_table[360], cols_end_table[360];
+		for (int theta = 0; theta < 360; theta++)
+		{
+			int theta_cos = r*cos(theta*3.1415 / 180);
+			int theta_sin = r*sin(theta*3.1415 / 180);
+
+			Mat subImg1, subImg2;
+			subImg1 = sumImg(Range(r, rows - r), Range(r, cols - r));
+			subImg2 = originImg(Range(r + theta_sin, rows - r + theta_sin), Range(r + theta_cos, cols - r + theta_cos));
+			subImg1 = subImg1 + subImg2;
+		}
+		aveImg[r] = sumImg / 360;
+
+		if (r >= min_radius + STEP)
+		{
+			int k = r - STEP;
+
+			diffImg[k].create(rows, cols, CV_16UC3);
+			diffImg[k].setTo(0);
+			absdiff(aveImg[k - STEP](Range(r, rows - r), Range(r, cols - r)), aveImg[k + STEP](Range(r, rows - r), Range(r, cols - r)), diffImg[k](Range(r, rows - r), Range(r, cols - r)));
+			vector<Mat> diffSplitedImg;
+			split(diffImg[k], diffSplitedImg);
+			diffImg[k] = (diffSplitedImg.at(0) + diffSplitedImg.at(1) + diffSplitedImg.at(2));
+			diffImg[k] = diffImg[k] - diffThresholdImg * 3;
+
+			//aveImg[k - STEP].release();
+		}
+	}
+
+	for (int r = min_radius; r <= max_radius; r++)
+	{
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < cols; j++)
+			{
+				uint16_t center_value = *((uint16_t*)diffImg[r].data + i*cols + j);
+				if (center_value <= 0) continue;
+
+				/***********************************************************/
+				/*
+				could be faster
+				*/
+				if (r + STEP > i || r + STEP > rows - i || r + STEP > j || r + STEP > cols - j) continue;
+				/***********************************************************/
+
+				int sub_r_start, sub_r_end;
+				if (r - min_radius_dist >= min_radius) sub_r_start = r - min_radius_dist;
+				else sub_r_start = min_radius;
+				if (r + min_radius_dist <= max_radius) sub_r_end = r + min_radius_dist;
+				else sub_r_end = max_radius;
+
+				int sub_i_start, sub_i_end;
+				if (i - min_center_dist >= 0) sub_i_start = i - min_center_dist;
+				else sub_i_start = 0;
+				if (i + min_center_dist <= rows) sub_i_end = i + min_center_dist;
+				else sub_i_end = rows;
+
+				int sub_j_start, sub_j_end;
+				if (j - min_center_dist >= 0) sub_j_start = j - min_center_dist;
+				else sub_j_start = 0;
+				if (j + min_center_dist <= cols) sub_j_end = j + min_center_dist;
+				else sub_j_end = cols;
+
+				int flag = 0;
+
+				//
+				for (int sub_r = sub_r_start; sub_r <= sub_r_end; sub_r++)
+				{
+					for (int sub_i = sub_i_start; sub_i < sub_i_end; sub_i++)
+					{
+						for (int sub_j = sub_j_start; sub_j < sub_j_end; sub_j++)
+						{
+							uint16_t surround_value = *((uint16_t*)diffImg[sub_r].data + sub_i*cols + sub_j);
+							if (surround_value > center_value) flag = 1;
+							if (flag == 1)break;
+						}
+						if (flag == 1)break;
+					}
+					if (flag == 1)break;
+				}
+
+				//
+				for (int p = 0; p < _circle_cnt; p++)
+				{
+					if (((j - x[p])*(j - x[p]) + (i - y[p])*(i - y[p])) < min_center_dist*min_center_dist && abs(radius[p] - r) < min_radius_dist)
+					{
+						if (center_value <= diff[p])
+						{
+							flag = 1;
+							continue;
+						}
+					}
+				}
+
+				if (flag == 0)
+				{
+					x[_circle_cnt] = j;
+					y[_circle_cnt] = i;
+					radius[_circle_cnt] = r;
+					diff[_circle_cnt] = center_value;
+					_circle_cnt++;
+				}
+
+				if (_circle_cnt == max_circle)
+				{
+					*circle_cnt = _circle_cnt;
+					return MY_OK;
+				}
+			}
+		}
+	}
+
+	*circle_cnt = _circle_cnt;
+	return MY_OK;
+}
+#endif // VERSION_6
 
 #ifdef VERSION_5
 /*
@@ -38,6 +227,15 @@ int ustc_Find_Circles_By_Difference(
 	if (min_radius < MIN_RASIUS) return MY_FAIL;
 	if (min_radius > max_radius) return MY_FAIL;
 	if (max_radius > MAX_RASIUS) return MY_FAIL;
+
+	if (max_radius > rows / 2) max_radius = rows / 2;
+	if (max_radius > cols / 2) max_radius = cols / 2;
+	if (min_radius > rows / 2) min_radius = rows / 2;
+	if (min_radius > cols / 2) min_radius = cols / 2;
+
+	if (min_center_dist < 0) return MY_FAIL;
+	if (min_radius_dist < 0) return MY_FAIL;
+	if (max_circle_diff <= 0 || max_circle_diff > 255) return MY_FAIL;
 
 	if (max_circle <= 0) return MY_FAIL;
 
@@ -119,7 +317,7 @@ int ustc_Find_Circles_By_Difference(
 		{
 			int k = r - STEP;
 
-			diffImg[k] = (aveImg[k - STEP] - aveImg[k + STEP]) + (aveImg[k + STEP] - aveImg[k - STEP]);
+			absdiff(aveImg[k - STEP], aveImg[k + STEP], diffImg[k]);
 			vector<Mat> diffSplitedImg;
 			split(diffImg[k], diffSplitedImg);
 			diffImg[k] = (diffSplitedImg.at(0) + diffSplitedImg.at(1) + diffSplitedImg.at(2));
